a383f2ed478256d3bae889f8ff51f77f826b7841
Lao Rivera <laorivera4@gmail.com>
2025-03-10 18:32:40 -0300

    Fix boxes apicall, add helmet, chest ,gloves boxes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
-import { CharacterBoxComponent } from './character-box/character-box.component';
+import { HeadBoxComponent } from './head-box/head-box.component';
+import { ChestBoxComponent } from './chest-box/chest-box.component'; 
+import { GlovesBoxComponent } from './gloves-box/gloves-box.component';// Updated import path
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CharacterBoxComponent],
+  imports: [ HeadBoxComponent, ChestBoxComponent, GlovesBoxComponent ], // Already correct, no change needed here
   template: `
-  <h1>Character Builder</h1>
-  <app-character-box></app-character-box>
+    <h1> ----> </h1> <!-- Updated title to be more generic -->
+
+    <app-head-box></app-head-box> 
+    <app-chest-box></app-chest-box>
+    <app-gloves-box></app-gloves-box> 
   `,
 })
-
 export class AppComponent {}
